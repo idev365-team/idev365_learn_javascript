d3e41a4103a8eb492a09b6b687d4451471b08a48
colin3dmax <colin@qidea.cc>
2019-01-24 19:13:17 +0800

    【跟山地人学JavaScript系列教程】6-10课

---
+++ Lesson02/main.js
@@ -0,0 +1,6 @@
+var para = document.querySelector('p');
+    para.addEventListener('click',updateName);
+    function updateName(){
+        var name = prompt('输入一个新的名字：');
+        para.textContent = "玩家1:"+name;
+    }
